04485014ac64260bb8f92cd242876d02a4e53fd0
Michael Zwyssig <mzwyssig@3ne.ch>
2018-10-02 11:47:42 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { InterfaceModule } from './shared/interface/interface.module';
+import { EntryModule } from './modules/entry/entry.module';
+import { AdminModule } from './modules/admin/admin.module';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    EntryModule,
+    AdminModule,
+    InterfaceModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
